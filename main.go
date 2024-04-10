f560de60befd8e218d1c6fbd3346fcf940ce9a00
Amiy Kumar <amiy90067@gmail.com>
2024-04-10 14:55:03 +0530

    Add support for profiling the code.

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"runtime/pprof"
 	"tree_lox/interpreter"
 	"tree_lox/parser"
 )
 
+// Maintain the interpreter state by making it global throughout the session.
 var lox_interpreter interpreter.Interpreter = interpreter.MakeInterpreter()
 
 func main() {
+	// Start CPU profile if enabled via the env-var CPUPROFILE.
+	if prof_out, has := os.LookupEnv("CPUPROFILE"); has && prof_out != "" {
+		f, err := os.Create(prof_out)
+		if err != nil {
+			log.Fatalf(
+				"Cannot create profile output file: '%v' (%v).\n",
+				prof_out, err,
+			)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	switch len(os.Args) {
 	case 0, 1:
 		execPrompt()
@@ -18,6 +34,7 @@ func main() {
 		execFromFile(os.Args[1])
 
 	default:
+		fmt.Fprintf(os.Stderr, "Usage: %v [filename]\n", os.Args[0])
 		os.Exit(1)
 	}
 }
@@ -26,7 +43,7 @@ func execFromFile(filepath string) {
 	source, err := os.ReadFile(filepath)
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot open file becase: %v.\n", err.Error())
+		fmt.Fprintf(os.Stderr, "Cannot open file '%v'  (%v).\n", filepath, err.Error())
 		os.Exit(1)
 	}
 
@@ -37,7 +54,6 @@ func execFromFile(filepath string) {
 	}
 
 	lox_interpreter.Interpret(stmts)
-
 }
 
 func execPrompt() {
